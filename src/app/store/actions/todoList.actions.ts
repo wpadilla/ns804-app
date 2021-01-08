f35b5d1365022bac72ed1e908f622c4409ef44e1
Williams <wilfre_padilla@hotmail.com>
2021-01-07 23:47:21 -0400

    Feat: create todoList action file Feat: add TodoList enum

---
+++ src/app/store/actions/todoList.actions.ts
@@ -0,0 +1,5 @@
+export enum TodoList {
+  LOAD_TODO_LIST = '[TODO LIST] Load Action',
+  LOAD_TODO_LIST_SUCCESS = '[TODO LIST] Load Action Success',
+  LOAD_TODO_LIST_FAILURE = '[TODO LIST] Load Action Failure',
+};
