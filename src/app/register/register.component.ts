4313ee177b18f85336d648c72ec204e9d327ea74
Williams <wilfre_padilla@hotmail.com>
2021-01-07 22:33:28 -0400

    Feat: html input were added. Feat: getting password and email values

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  email: FormControl = new FormControl();
+  password: FormControl = new FormControl();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  register() {
+    // code to log in
+  }
 }
