5d3c6604905407632f44050d40c57b69135b9fc7
Williams <wilfre_padilla@hotmail.com>
2021-01-08 08:41:00 -0400

    Feat add todo service

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Observable } from 'rxjs';
 import TokenService from './token.service';
+import TodoEntity from '../store/models/todo.model';
 
 @Injectable({
   providedIn: 'root'
@@ -27,6 +28,15 @@ export class TodoService extends TokenService{
 
   public deleteTodo(_id: string): Observable<any> {
     return this.http.delete(`http://localhost:3000/api/todos/${_id}`,
+      {
+        headers: {
+          Authorization: this.token,
+        }
+      });
+  }
+
+  public addTodo(todo: TodoEntity): Observable<any> {
+    return this.http.post(`http://localhost:3000/api/todos/`, todo,
       {
         headers: {
           Authorization: this.token,
