de720fc03e7556ca7143da4c364e1f0a8cd59c55
Williams <wilfre_padilla@hotmail.com>
2021-01-08 04:46:22 -0400

    Feat: todo list actions types

--- src/app/store/actions/todoList.actions.ts
+++ src/app/store/actions/todoList.actions.ts
@@ -1,5 +1,24 @@
-export enum TodoList {
+import Action from '../models/action.model';
+import TodoEntity from '../models/todo.model';
+
+export enum TodoListActionTypes {
   LOAD_TODO_LIST = '[TODO LIST] Load Action',
   LOAD_TODO_LIST_SUCCESS = '[TODO LIST] Load Action Success',
   LOAD_TODO_LIST_FAILURE = '[TODO LIST] Load Action Failure',
+}
+
+
+export class LoadTodoListAction implements Action {
+  readonly type = TodoListActionTypes.LOAD_TODO_LIST;
+}
+export class LoadTodoListSuccessAction implements Action {
+  readonly type = TodoListActionTypes.LOAD_TODO_LIST_SUCCESS;
+
+  constructor(public payload: TodoEntity[]) {}
+
+}
+export class LoadTodoListFailureAction implements Action {
+  readonly type = TodoListActionTypes.LOAD_TODO_LIST_FAILURE;
+
+  constructor(public payload: Error) {}
 }
